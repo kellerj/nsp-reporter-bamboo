44727c48064192d7c77a9bef80ab98138c80d216
Jonathan Keller <keller.jonathan@gmail.com>
2017-11-27 13:09:01 -0800

    Initial version of the bamboo formatter

---
+++ src/reporter.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+import { format } from 'util';
+
+const startTime = new Date();
+
+export function error(err) {
+  console.error(err.toString());
+}
+
+export function success(result) {
+  console.log(result);
+}
+
+export const check = {};
+
+check.success = function checkSuccess(result) {
+  const summary = {
+    stats: {
+      tests: 0,
+      passes: 0,
+      failures: 0,
+      duration: Date.now() - startTime,
+      start: startTime,
+      end: new Date(),
+    },
+    failures: [],
+    passes: [],
+    skipped: [],
+  };
+  result.data.forEach((validationFailure) => {
+    summary.failures.push({
+      title: validationFailure.title,
+      fullTitle: `${validationFailure.ancestorTitles} ${validationFailure.title}`,
+      duration: 0,
+      errorCount: 1,
+      error: format(
+        'Module %s has a known vulnerability: "%s" (vulnerable: %s, patched: %s, yours: %s), see %s',
+        validationFailure.module, validationFailure.title, validationFailure.vulnerable_versions,
+        validationFailure.patched_versions, validationFailure.version, validationFailure.advisory,
+      ),
+    });
+    summary.stats.tests += 1;
+    summary.stats.failures += 1;
+
+    console.log(JSON.stringify(summary, null, 2));
+  });
+};
